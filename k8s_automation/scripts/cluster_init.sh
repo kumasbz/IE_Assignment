150a66fcb868bc49c9a2f3e27791d763c188736a
kumasbz <shubhamk137@gmail.com>
2024-09-08 16:48:23 +0530

    Print Join command for worker nodes

--- k8s_automation/scripts/cluster_init.sh
+++ k8s_automation/scripts/cluster_init.sh
@@ -11,4 +11,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # Install Flannel network plugin
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-echo "Cluster initialized. You can now join worker nodes."
+echo "Cluster initialized. You can now join worker nodes using the command below."
+
+# Print Join command for worker nodes
+kubeadm token create --print-join-command
